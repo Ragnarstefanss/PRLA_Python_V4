a13701387aacc0da61e4d7c4689ed8cad255eeb5
Kiddi <kiddinn95@gmail.com>
2016-12-11 06:28:39 +0000

    progress being made

--- clean.py
+++ clean.py
@@ -1,5 +1,55 @@
 import os
+import re
+import string
+import json
+import urllib
+import shutil
 def clean(downloads, sorted):
+    tvList = []
     for subdir, dirs, files in os.walk(downloads):
-        nth = ""
+        for file in files:
+            name = file.title()
+            match = re.compile("(?!.*(\.[Jj]pg|\.[Rr]ar|\.[Zz]ip|\.[Mm]p3|\.[Ii]gnore|\.[Nn]fo|.[R]\d{1,3}|\.[Dd]at|\.[Pp]ng|\.[Ll]nk|"
+                               "\.[Pp]ar[t\d*]|\.[Ss]vf|\.[Ss]fv|\.[Mm]ta|\.[Tt]xt|\.[Ww]av))[Ss]?\d{1,2}x?[Ee]?[^\d\._ -]\d{1,3}")
+            if match.search(name):
+                tvList.append(name)
+                shutil.move(os.path.join(subdir, file), "/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted/TV_shows/")
+    for show in tvList:
+        print(show)
     return
+
+#
+    # def main(mypath, destination):
+    # myndir = []
+    # listi = []
+    # strengur = ''
+
+    # for (dirpath, dirnames, filenames) in os.walk(mypath):
+    #     myndir.extend(filenames)
+    #     break
+
+    ##    for l in myndir:
+    ##        print (l)
+    ##
+    # print(myndir)
+
+    # name = "12 Years a Slave"
+    # year = 0
+    # if year != 0:
+     #    url = "http://www.omdbapi.com/?t=" + name + "&y=" + str(year)
+    # else:
+     #    url = "http://www.omdbapi.com/?t=" + name
+
+    # url_values = json.loads(urllib.urlopen(url).read())
+
+
+
+# Fá nafn á mynd/þætti
+# fara í gegnum allar undir möppur
+# Leita í omdbapi að nafninu með search string
+# skoða "Type": "series" / "Type": "movies"
+# ef bíó mynd setja skránna inn í ../temp/movies
+# ef sjónvarpsþáttur setja skránna inn í ( ../temp/series/[SHOW_NAME]/[SEASON_NUMBER]/[episode] )
+
+
+# ef skrá ekki lesanleg þá má eyða henni (aka .jpg , .rar, .zip)
--- testV4.py
+++ testV4.py
@@ -1,11 +1,12 @@
 import shutil
 import os
 import zipfile
-#import clean
+import clean
 shutil.rmtree("downloads")
 shutil.rmtree("sorted")
 os.makedirs("sorted")
+os.makedirs("sorted/TV_shows")
 zip = zipfile.ZipFile("downloads.zip")
 zip.extractall()
 zip.close()
-#clean.clean("/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/downloads", "/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted")
+clean.clean("downloads", "sorted")
