c90e5cd1fe85a2d0ca7e752524ee00dc3790efda
None None <None None>
2016-12-11 15:31:51 +0000

    comments from bottom of clean are in this file

---
+++ comments.py
@@ -0,0 +1,35 @@
+#
+    # def main(mypath, destination):
+    # myndir = []
+    # listi = []
+    # strengur = ''
+
+    # for (dirpath, dirnames, filenames) in os.walk(mypath):
+    #     myndir.extend(filenames)
+    #     break
+
+    ##    for l in myndir:
+    ##        print (l)
+    ##
+    # print(myndir)
+
+    # name = "12 Years a Slave"
+    # year = 0
+    # if year != 0:
+     #    url = "http://www.omdbapi.com/?t=" + name + "&y=" + str(year)
+    # else:
+     #    url = "http://www.omdbapi.com/?t=" + name
+
+    # url_values = json.loads(urllib.urlopen(url).read())
+
+
+
+# Fá nafn á mynd/þætti
+# fara í gegnum allar undir möppur
+# Leita í omdbapi að nafninu með search string
+# skoða "Type": "series" / "Type": "movies"
+# ef bíó mynd setja skránna inn í ../temp/movies
+# ef sjónvarpsþáttur setja skránna inn í ( ../temp/series/[SHOW_NAME]/[SEASON_NUMBER]/[episode] )
+
+
+# ef skrá ekki lesanleg þá má eyða henni (aka .jpg , .rar, .zip)
