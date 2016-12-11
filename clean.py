59edd982923b8aaf152cc1c14be51346740d9598
Kiddi <kiddinn95@gmail.com>
2016-12-11 07:13:29 +0000

    If an unimplemented function is made and it works, the rest of this *should* work as intended

--- clean.py
+++ clean.py
@@ -1,8 +1,8 @@
 import os
 import re
-import string
-import json
-import urllib
+#import string
+#import json
+#import urllib
 import shutil
 def clean(downloads, sorted):
     tvList = []
@@ -13,11 +13,21 @@ def clean(downloads, sorted):
                                "\.[Pp]ar[t\d*]|\.[Ss]vf|\.[Ss]fv|\.[Mm]ta|\.[Tt]xt|\.[Ww]av))[Ss]?\d{1,2}x?[Ee]?[^\d\._ -]\d{1,3}")
             if match.search(name):
                 tvList.append(name)
-                if not os.path.exists("/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted/TV_shows/"+name):
-                    shutil.move(os.path.join(subdir, file), "/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted/TV_shows/")
+                newPath = processTvShowName(name)
+                if not os.path.exists("/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted/TV_shows/"+newPath):
+                    shutil.move(os.path.join(subdir, file), "/Users/kristinn/PycharmProjects/PRLA/PRLA_Python_V4/sorted/TV_shows/"+newPath)
     for show in tvList:
         print(show)
     return
+
+def processTvShowName(name):
+    #TODO: implement
+    #Endurformatta nafn þáttar til að losna við punkta og strik og slíkt úr nafni, nafn = allt á undan S01E01 etc.
+    #Skila nafni skráar fegruðu, nafni þáttar og seríunúmeri
+    #Helst formatta skilagildi sem enda áframhald á pathi frá TV_shows
+    return name
+    
+
 
 #
     # def main(mypath, destination):
